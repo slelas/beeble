7ef7f8ac30bf82a602216d8cb74b0f2eb38d5225
zvonimir-delas <6d.solin@gmail.com>
2018-01-11 23:54:19 +0100

    Fix header for signup and profile route

--- Beeble.Web/scripts/app/routes/signup/signup.route.js
+++ Beeble.Web/scripts/app/routes/signup/signup.route.js
@@ -3,6 +3,12 @@
         .state('signup', {
             url: '/signup',
             controller: 'signupController',
-            templateUrl: 'scripts/app/routes/signup/signup.template.html'
+            templateUrl: 'scripts/app/routes/signup/signup.template.html',
+            onEnter: function ($rootScope) {
+                $rootScope.isPageWithValidation = true;
+            },
+            onExit: function ($rootScope) {
+                $rootScope.isPageWithValidation = false;
+            }
         });
 });
