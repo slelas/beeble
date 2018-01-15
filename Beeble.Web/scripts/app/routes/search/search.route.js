dd09e831c12ea010c25dec2aef1ade756ebc5f19
Stipe Lelas <stipe@dump.hr>
2018-01-15 19:43:01 +0100

    Finished mobile version of search results

--- Beeble.Web/scripts/app/routes/search/search.route.js
+++ Beeble.Web/scripts/app/routes/search/search.route.js
@@ -1,7 +1,7 @@
 ﻿angular.module('myApp').config(function ($stateProvider) {
 	$stateProvider
 		.state('search', {
-			url: '/search',
+			url: '/',
 			controller: 'searchController',
 			templateUrl: 'scripts/app/routes/search/search.template.html',
 			parent: 'userLoggedIn',
