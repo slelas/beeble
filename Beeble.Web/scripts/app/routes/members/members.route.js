74b9a15222f5f97ee4c95bf369971ee133ab0098
Stipe Lelas <stipe@dump.hr>
2018-01-31 18:56:00 +0100

    Finished members list and mobile version of lend-and-return

---
+++ Beeble.Web/scripts/app/routes/members/members.route.js
@@ -0,0 +1,8 @@
+﻿angular.module('myApp').config(function ($stateProvider) {
+	$stateProvider
+		.state('members', {
+			url: '/members',
+			controller: 'membersController',
+			templateUrl: 'scripts/app/routes/members/members.template.html'
+		});
+});
