50fc6cd01614feca0e608ffa4185575ccecbedad
Stipe Lelas <stipe@dump.hr>
2018-02-03 01:27:50 +0100

    General fixes

--- Beeble.Data/BeebleInitializer.cs
+++ Beeble.Data/BeebleInitializer.cs
@@ -8,13 +8,11 @@ using Beeble.Data.Models;
 
 namespace Beeble.Data
 {
-    //public class BeebleInitializer : DropCreateDatabaseAlways<AuthContext>
-    public class BeebleInitializer : DropCreateDatabaseIfModelChanges<AuthContext>
+    public class BeebleInitializer : DropCreateDatabaseAlways<AuthContext>
+    // public class BeebleInitializer : DropCreateDatabaseIfModelChanges<AuthContext>
     {
         protected override void Seed(AuthContext context)
         {
-
-
             base.Seed(context);
         }
     }
