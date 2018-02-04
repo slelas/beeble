48e02411546f7070a3ca1dbd3a0769e2f59a27f6
Stipe Lelas <stipe@dump.hr>
2018-02-04 12:44:30 +0100

    Statistics fix

--- Beeble.Web/scripts/app/routes/statistics/statistics.controller.js
+++ Beeble.Web/scripts/app/routes/statistics/statistics.controller.js
@@ -5,6 +5,8 @@
     $scope.labels2 = ['Siječanj', 'Veljača', 'Ožujak', 'Travanj', 'Svibanj', 'Lipanj', 'Srpanj', 'Kolovoz', 'Rujan', 'Listopad', 'Studeni', 'Prosinac'];
     $scope.series = ['Posuđene knjige', 'Rezervirane knjige'];
 
+    $scope.colours = ['#B59A56', '#7F6C3D', '#FFD979', '#40361E', '#E5C36D', '#7F6739'];
+
     $scope.data2 = [
     [32, 45, 78, 28, 25, 50, 60, 63, 90, 48, 66, 69],
     [15, 28, 23, 68, 58, 38, 77, 93, 19, 23, 78, 50]
@@ -15,7 +17,7 @@
     $scope.data3 = [
         [10, 15, 23, 28, 34, 48, 72]
     ];
-    
+
     $scope.onClick = function (points, evt) {
         console.log(points, evt);
     };
